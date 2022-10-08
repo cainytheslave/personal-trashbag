f086c599884e5dc1bed1c1002ff4b57f499c0c5c
John Wagner <info@techbra.in>
2022-10-08 22:42:12 +0200

    Added opengraph

---
+++ resources/views/components/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Windows 97</title>
+
+    <meta property="og:title" content="Windows 97 - Never existed thus superior">
+    <meta property="og:site_name" content="Johns personal trashpage">
+    <meta property="og:url" content="https://win97.de">
+    <meta property="og:description" content="">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="{{ Vite::asset('images/logo.svg') }}">
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body class="antialiased">
+
+    {{ $slot }}
+
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,27 +1,3 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<x-app>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Windows 97</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <style>
-        body {
-            font-family: 'Nunito', sans-serif;
-        }
-    </style>
-</head>
-
-<body class="antialiased p-4">
-    <h1 class="text-4xl mb-4 text-center font-semibold">Hi Mom</h1>
-    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/6qLEOZvf5gI7kWE63JE7p3?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-</body>
-
-</html>
+</x-app>
